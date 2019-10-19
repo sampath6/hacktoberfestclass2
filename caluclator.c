46a044211722d8f73b8195c356ae16d00d49faa5
sampath <sampathkumarreddy.m19@iiits.in>
2019-10-19 19:15:18 +0530

    made a calci but not the sum

---
+++ caluclator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int product(int a,int b)
+{
+return a*b;
+
+}
+int diff(int a,int b)
+{
+return (a-b);
+}
+int main()
+{
+int  a=5;b=2;
+printf("product is %d\ndifference is %d\n",product(a,b),diff(a,b));
+return 0;
+
+}
