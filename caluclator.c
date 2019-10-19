e6da74d46b5d61da8b100a5bf8a7090a8cac2b6c
sampath <sampathkumarreddy.m19@iiits.in>
2019-10-19 19:23:13 +0530

    modified

--- caluclator.c
+++ caluclator.c
@@ -8,10 +8,14 @@ int diff(int a,int b)
 {
 return (a-b);
 }
+int sum(int a,int b)
+{
+return a+b;
+}
 int main()
 {
 int  a=5;b=2;
-printf("product is %d\ndifference is %d\n",product(a,b),diff(a,b));
+printf("product is %d\ndifference is %d\n%d is sum",product(a,b),diff(a,b),sum(a,b));
 return 0;
 
 }
